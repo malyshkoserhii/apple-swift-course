e96ab9a526158edb5f23bcfb7bf331ec26135d1d
malyshkoserhii <malyshkosergiy@gmail.com>
2026-03-28 11:57:01 +0200

    feat: Added Pick-a-Pal

---
+++ Pick-a-Poll/Pick-a-Poll/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  Pick-a-Poll
+//
+//  Created by Serhii Malyshko on 28.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var names: [String] = []
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pall")
+            }
+            .font(.title)
+            .bold()
+            
+            Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
+            
+            List {
+                ForEach(names, id: \.description) { name in
+                    Text(name)
+                }
+            }
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Divider()
+            
+            Toggle("Remove when picked", isOn: $shouldRemovePickedName)
+            
+            Button {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                } else {
+                    pickedName = ""
+                }
+                
+            } label: {
+                Text("Pick Random Name")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick-a-Poll/Pick-a-Poll/Pick_a_PollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PollApp.swift
+//  Pick-a-Poll
+//
+//  Created by Serhii Malyshko on 28.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
